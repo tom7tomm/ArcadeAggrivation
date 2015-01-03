383a0b6f129e94938d5a2739e1faa4daf19feacf
tom7tomm <contact.tom7tomm@gmail.com>
2015-01-03 11:04:55 +0000

    Added Jim
    Implemented the basic drawing of Jim, and allowed him to move about on
    the x axsis.

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
-function love.load()
+require "obj/jim"
 
+function love.load()
+	love.graphics.setBackgroundColor(0, 0, 0)
+	jim.load()
 end
 
 function love.update(dt)
-
+	jim.updateAll(dt)
 end
 
 function love.draw()
-
+	jim.drawAll()
 end
